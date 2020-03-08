0b70c0b7ed437ac29bf1872eafe39d637d283208
Anshul Goyal <anshulgoel151999@gmail.com>
2020-03-08 20:52:00 +0530

    Parallax

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import classes from './card.module.css'
 
-const Card = () => {
+const Card = ({ isOpen = false } = {}) => {
     const colors = ["#4ab7cc", " #779233", "#38465b"]
     return (
-        <div className={classes.card} style={{ backgroundColor: colors[Math.floor(Math.random() * 3)] }}>
+        <div className={classes.card} style={{ backgroundColor: colors[Math.floor(Math.random() * 3)], height: !isOpen ? '250px' : "" }}>
             <div className={classes.number_container}> <h2 className={classes.number}>01.</h2></div>
             <div></div><div></div>
             <div className={classes.title}>Xunbao</div><div></div><div></div>
-            <div className={classes.subtitle}>Read More</div><div></div><div className={classes.plus}>+</div>
+            {!isOpen ? <><div className={classes.subtitle}>Read More</div> <div></div> <div className={classes.plus}>+</div></> : ""}
+            {isOpen ?
+                <div className={classes.text}>
+                    {"Design, law, construction, asset management, financial and professional services consultancies all have to manage their professional indemnity and D&O liabilities. We have an in-depth knowledge not just of these industries, but also the constantly changing regulatory landscape, and the increasingly competitive environment they work within."}
+                </div> : ""}
         </div>
     )
 }
--- src/pages/events.js
+++ src/pages/events.js
@@ -2,23 +2,22 @@ import BackgroundImage from "../images/bgimage.png"
 import classes from "./event.module.css"
 import React from 'react'
 import Card from '../components/card'
+import { Parallax } from "react-parallax"
 
 const Events = ({ data } = { data: [] }) => {
-    return (<div className={classes.bg} style={
-        {
-            backgroundImage: `url(${BackgroundImage})`,
-        }}>
-
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-    </div>)
+    return (<Parallax bgImage={BackgroundImage} strength={800} >
+        <div className={classes.bg} >
+            <Card></Card>
+            <Card isOpen={true}></Card>
+            <Card></Card>
+            <Card isOpen={true}></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card isOpen={true}></Card>
+            <Card></Card>
+        </div>
+    </Parallax>)
 }
 
 export default Events;
